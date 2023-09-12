e5e6350632786502810b97258f147d556fef5f28
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-09-12 19:36:28 -0300

    clean coding the input

---
+++ src/data/formFields.js
@@ -0,0 +1,11 @@
+const formFields = [
+  { label: "Name", type: "text", value: "" },
+  { label: "Last Name", type: "text", value: "" },
+  { label: "Email", type: "email", value: "" },
+  { label: "Password", type: "password", value: "" },
+  { label: "Confirm Password", type: "password", value: "" },
+  { label: "City", type: "text", value: "" },
+  { label: "State", type: "text", value: "" },
+];
+
+export default formFields;
--- src/views/FormPage.vue
+++ src/views/FormPage.vue
@@ -1,26 +1,22 @@
 <template>
   <form @submit.prevent="submitForm">
-    <base-input label="Name" type="text" @input="onNameInput"></base-input>
     <base-input
-      label="Last Name"
-      type="text"
-      @input="onLastNameInput"
+      v-for="(field, index) in formFields"
+      :key="index"
+      :label="field.label"
+      :type="field.type"
+      @input="onInput(index, $event)"
+      v-model="field.value"
     ></base-input>
-    <base-input label="Email" type="email" @input="onEmailInput"></base-input>
-    <base-input
-      label="Password"
-      type="password"
-      @input="onPasswordInput"
-    ></base-input>
-    <base-input label="City" type="text" @input="onCityInput"></base-input>
-
-    <the-button @click="submitForm($event)">Cadastrar</the-button>
+    <the-button type="submit">Cadastrar</the-button>
   </form>
 </template>
 
 <script>
 import BaseInput from "@/components/atoms/BaseInput.vue";
 import TheButton from "@/components/atoms/TheButton.vue";
+import formFields from "@/data/formFields.js";
+
 export default {
   name: "FormPage",
   components: {
@@ -29,37 +25,17 @@ export default {
   },
   data() {
     return {
-      event: {
-        name: "",
-        lastName: "",
-        city: "",
-        email: "",
-        password: "",
-      },
+      formFields: formFields,
     };
   },
   methods: {
-    onNameInput(event) {
-      console.log("Name input value:", event.target.value);
-    },
-    onLastNameInput(event) {
-      console.log("Last Name input value:", event.target.value);
-    },
-    onCityInput(event) {
-      console.log("City input value:", event.target.value);
-    },
-    onPasswordInput(event) {
-      console.log("Password input value:", event.target.value);
-    },
-    onEmailInput(event) {
-      console.log("Email input value:", event.target.value);
+    onInput(index, event) {
+      this.formFields[index].value = event.target.value;
     },
     submitForm() {
-      console.log("Name input value:", this.event.name);
-      console.log("Last Name input value:", this.event.lastName);
-      console.log("City input value:", this.event.city);
-      console.log("Password input value:", this.event.password);
-      console.log("Email input value:", this.event.email);
+      for (const field of this.formFields) {
+        console.log(`${field.label} input value:`, field.value);
+      }
     },
   },
 };
