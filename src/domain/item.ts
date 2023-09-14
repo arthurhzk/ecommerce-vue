9ae4470c49cdb8aa1b063f2c27a91439861e0206
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-09-14 09:18:19 -0300

    typescript added

---
+++ src/domain/item.ts
@@ -0,0 +1,7 @@
+// id: 2,
+// title: "Asus ROG Gaming",
+// price: 1000.0,
+// image:
+//   "https://a-static.mlcdn.com.br/280x210/placa-mae-asus-rog-strix-b550-e-gaming-amd-am4-atx-ddr4/kabum/113864/f49dc2e476249d919350478bb9576120.jpeg",
+// description:
+//   "O Asus ROG Gaming é uma placa-mãe de alta qualidade projetada para gamers exigentes. Com suporte para processadores AMD AM4 e tecnologia DDR4, esta placa oferece desempenho excepcional para jogos intensos. Além disso, possui iluminação RGB personalizável para uma aparência incrível em seu gabinete de PC.",
