54e0f6a9984faddbbae38c047243d421ce0b3230
Arthur Kunz <arthur.kunz@universo.univates.br>
2023-09-15 18:26:43 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,13 @@ export default {
   },
 };
 </script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
